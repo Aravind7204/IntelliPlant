2bbad5470bc3cce9e72cbabd8b5621b13f50d32a
Aravind7204 <107171394+Aravind7204@users.noreply.github.com>
2024-12-21 14:42:44 +0530

    app.py

--- app.py
+++ app.py
@@ -120,4 +120,5 @@ def get_solution():
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=8000)
+
